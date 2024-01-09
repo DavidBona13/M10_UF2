0520131351b4a34a4f3405464c1f801c45a0eb21
David Hernández Bona <2023.David.Hernandez@itibcn.cat>
2024-01-09 20:54:13 +0100

    dia 1

---
+++ Repàs/exercici5.py
@@ -0,0 +1,11 @@
+def main():
+    
+    h1 = int(input("Siusplau introdueixi el valor: "))
+    n = h1 % 2
+    if n == 0:
+        print("El número és parell")
+    else:
+        print("El número és senar")
+        
+if __name__ == "__main__":
+    main()
---
+++ Repàs/exercici6.py
@@ -0,0 +1,9 @@
+def main():
+    
+    i= 1
+    while i <= 100:
+        print(i)
+        i += 1
+        
+if __name__ == "__main__":
+    main()
