e44483bf7232b1a3e57b1e87690e35dbcea54148
David Hernández Bona <2023.David.Hernandez@itibcn.cat>
2024-02-04 18:57:52 +0100

    acabat de comentar

--- Practica2/delete.py
+++ Practica2/delete.py
@@ -2,10 +2,13 @@ from read import consultar2
 
 def eliminar(connection, con_cursor):
      a = True
+     #Primer fem la comprovació per si el sintetitzador es o no dins la base de dades. Si no hi és, s'ha de tornar a buscar un altre sintetitzador.
      while a:
+         #Comprovem si existeix cridant el mètode 'consultar2' que retorna el sintetitzador
         input1 = input("Si us plau, introdueix-hi el nom del sintetitzador que vols eliminar ")
         vari1 = consultar2(connection, con_cursor, input1)
         
+        #Si existeix entra al 'if' i et fa l'operació, sino, et diu que no existeix.
         if vari1:
             sql = """DELETE FROM public.sintetitzadors WHERE nom = %s"""
             con_cursor.execute(sql, (input1,))
