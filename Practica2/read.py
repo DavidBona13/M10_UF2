59d6e9bf638b403e62f01a059e16101c1d0e3a7f
David Hernández Bona <2023.David.Hernandez@itibcn.cat>
2024-02-04 19:01:17 +0100

    Mètode Read comentat

--- Practica2/read.py
+++ Practica2/read.py
@@ -1,9 +1,10 @@
+#Mètodo que et treu per terminal tots els sintetitzadors que hi han dins la base de dades.
 def consultar(connection, con_cursor):
     sql = """ SELECT id, nom, marca, tipus, descripcio, connexions FROM public.sintetitzadors"""
     
     con_cursor.execute(sql)
     myresult = con_cursor.fetchall()
-    
+    #Recorrer tots els elements de la taula
     for i in myresult:
         print("====================================================================================")
         print("Id: ", i[0])
@@ -13,8 +14,10 @@ def consultar(connection, con_cursor):
         print("Descripció: ", i[4])
         print("Tipus de conexions: ", i[5])
 
+#Aquest mètode de consulta serà útil per comprovar en altres mètodes si el sintetitzador ja existeix o no
 def consultar2(connection, con_cursor, input1):
     sql = """ SELECT id, nom, marca, tipus, descripcio, connexions FROM public.sintetitzadors WHERE nom = %s"""
     con_cursor.execute(sql, (input1,))
+    #En aquest cas s'utilitza el 'fetchone' perquè només volem retornar l'element buscat.
     myresult = con_cursor.fetchone()
     return myresult
