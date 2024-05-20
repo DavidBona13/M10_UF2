8356946cff03f21780d2b236673be2344b29b984
DavidBona13 <2023_David.Hernandez@iticbcn.cat>
2024-05-21 00:05:18 +0200

    Mòduls inventari_harmonia ventas_harmonia i facturacion_harmonia 'fets' falta seguretat

--- odoo/odoo-custom-addons/ventas_harmonia/__manifest__.py
+++ odoo/odoo-custom-addons/ventas_harmonia/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'ventas_harmonia', 'facturacion_harmonia'],
 
     # always loaded
     'data': [
@@ -32,4 +32,8 @@
     'demo': [
         'demo/demo.xml',
     ],
+
+    'installable': True,
+    'application': True,
 }
+
