5a861bb48080678f13091bc5f7d942eac3eeda47
DavidBona13 <2023_David.Hernandez@iticbcn.cat>
2024-05-20 02:01:51 +0200

    métodes noudedeesnous

--- odoo/odoo-custom-addons/inventarioHarmonia/__manifest__.py
+++ odoo/odoo-custom-addons/inventarioHarmonia/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Inventario",
+    'name': "inventarioHarmonia",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -33,4 +33,8 @@
     'demo': [
         'demo/demo.xml',
     ],
+
+    'installable': True,
+
+    'aplication': True,
 }
--- odoo/odoo-custom-addons/inventarioHarmonia/models/categoria.py
+++ odoo/odoo-custom-addons/inventarioHarmonia/models/categoria.py
@@ -6,3 +6,14 @@ class Categoria(models.Model):
 
     nom = fields.Char(string="Nombre categoría", required=True)
     descripcion = fields.Text(string="Descripción.")
+
+    @api.multi 
+    def consultar_prodCat(self, categoria_id):
+        productos = self.env['inventario.inventario'].search([('categoria_id', '=', categoria_id)])
+        return productos
+    
+    @api.multi 
+    def cantidad_prod(self, categoria_id):
+        productos = self.consultar_prodCat(categoria_id)
+        quantitat = sum(producto.cantidad for producto in productos)
+        return quantitat
