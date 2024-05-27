520c81954ff3ee771c4cc2977ef815d2a8a61ba2
DavidBona13 <2023_David.Hernandez@iticbcn.cat>
2024-05-28 00:33:44 +0200

    modificació errors validationerror i apimodel

--- odoo/odoo-custom-addons/inventario_harmonia/models/categoria.py
+++ odoo/odoo-custom-addons/inventario_harmonia/models/categoria.py
@@ -7,13 +7,14 @@ class Categoria(models.Model):
     nom = fields.Char(string="Nombre categoría", required=True)
     descripcion = fields.Text(string="Descripción.")
 
-    @api.multi 
+   
     def consultar_prodCat(self):
         productos = self.env['inventario_harmonia.inventario'].search([('categoria_id', '=', self.id)])
         return productos
     
-    @api.multi 
+    
     def cantidad_prod(self):
         productos = self.consultar_prodCat()
         quantitat = sum(producto.cantidad for producto in productos)
         return quantitat
+ 
